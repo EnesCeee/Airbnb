f437d47b78f235433612c9d05cacf80241066aa9
Enes Ceylan <enesceylan_6699@hotmail.com>
2023-11-17 14:00:02 +0300

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by Enes Ceylan on 17.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
